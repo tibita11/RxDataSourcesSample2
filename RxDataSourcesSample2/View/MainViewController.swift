c980129a785fdcf359678c0bea2952658e994687
tibita11 <tibita0811@gmail.com>
2023-02-17 17:03:43 +0900

    initial commit

---
+++ RxDataSourcesSample2/View/MainViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MainViewController.swift
+//  RxDataSourcesSample2
+//
+//  Created by 鈴木楓香 on 2023/02/17.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
